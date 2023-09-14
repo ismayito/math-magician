1aa16fef36ce306b609c3b92c6ad45fe3f069833
unknown <ismayito1@gmail.com>
2023-09-14 11:57:10 -0700

    Added calculator functionality

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,24 +1,28 @@
+import { useState } from 'react';
 import './Calculator.css';
 import ButtonsComponent from './ButtonsComponent';
+import calculate from '../logic/calculate';
+import ButtonsZeroComponent from './ButtonsZeroComponent';
 
-const Calculator = () => (
+function Calculator() {
+  const [screenDisplay, setScreenDisplay] = useState({ tota: 0, next: null, operation: null });
+  const handleOnClick = (value) => {
+    setScreenDisplay((prevScreenDisplay) => calculate(prevScreenDisplay, value));
+    calculate(value, value);
+  };
 
-  <div className="calculator-container">
-    <div className="screen">0</div>
-    <ButtonsComponent num1="AC" num2="+/-" num3="%" num4="/" />
-    <ButtonsComponent num1="7" num2="8" num3="9" num4="x" />
-    <ButtonsComponent num1="4" num2="5" num3="6" num4="-" />
-    <ButtonsComponent num1="1" num2="2" num3="3" num4="+" />
-    <table className="calculator">
-      <tr>
-        <td colSpan="2" className="zero-btn">
-          <button className="zero" type="button">0</button>
-        </td>
-        <td><button type="button">.</button></td>
-        <td><button type="button" className="signs">=</button></td>
-      </tr>
-    </table>
-  </div>
-);
+  return (
+    <div className="calculator-container">
+      <div className="screen">
+        {screenDisplay}
+      </div>
+      <ButtonsComponent num1="AC" num2="+/-" num3="%" num4="÷" onShow={handleOnClick} />
+      <ButtonsComponent num1="7" num2="8" num3="9" num4="x" onShow={handleOnClick} />
+      <ButtonsComponent num1="4" num2="5" num3="6" num4="-" onShow={handleOnClick} />
+      <ButtonsComponent num1="1" num2="2" num3="3" num4="+" onShow={handleOnClick} />
+      <ButtonsZeroComponent num1="0" num2="." num3="=" onShow={handleOnClick} />
+    </div>
+  );
+}
 
 export default Calculator;
